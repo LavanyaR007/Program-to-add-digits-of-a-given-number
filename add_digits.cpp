b78f909680b09e8d5262e971e4a42560eca96020
LavanyaR007 <75923059+LavanyaR007@users.noreply.github.com>
2020-12-13 11:19:13 +0530

    Create add_digits.cpp

---
+++ add_digits.cpp
@@ -0,0 +1,20 @@
+// Program to add digits of a given number
+# include<iostream>
+using namespace std;
+int main()
+{
+    int n,m,sum,rem;
+    cout<<"Enter the number:";
+    cin>>n;
+    m=n;
+    sum=0;
+    while(n!=0)
+    {
+        rem=n%10;
+        sum=sum+rem;
+        n=n/10;
+    }
+    cout<<"Sum of digits in "<<m<<" is "<<sum<<endl;
+    return 0;
+}
+
